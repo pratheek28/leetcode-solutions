aeaf5fe4ed35bbb8244294561aae74724f2af179
Pratheek Sunilkumar <66023535+pratheek28@users.noreply.github.com>
2026-07-25 19:28:59 -0700

    Create 72-EditDistance

---
+++ 72-EditDistance/72-EditDistance.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/25/2026, 7:28:59 PM
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
+
+        for (int i = 0; i < dp.size(); i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 0; i < dp[0].size(); i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < dp.size(); i++) {
+            for (int j = 1; j < dp[i].size(); j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = min(min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        return dp[word1.size()][word2.size()];
+    }
+};
