d8e49e73ae6581faacb721e01ebedf379ba5f6df
Pratheek Sunilkumar <66023535+pratheek28@users.noreply.github.com>
2026-07-26 18:53:59 -0700

    File updated at 7/26/2026, 6:53:58 PM

--- 78-Subsets/78-Subsets.cpp
+++ 78-Subsets/78-Subsets.cpp
@@ -1,20 +1,26 @@
-// Last updated: 7/26/2026, 6:43:21 PM
+// Last updated: 7/26/2026, 6:53:58 PM
 1class Solution {
-2    void subsets(vector<int>& nums, vector<vector<int>>& sol, vector<int>& curr, int start) {
-3        sol.push_back(curr);
-4
-5        for (int i = start; i < nums.size(); i++) {
-6            curr.push_back(nums[i]);
-7            subsets(nums, sol, curr, i + 1);
-8            curr.pop_back();
-9        }
-10    }
-11public:
-12    vector<vector<int>> subsets(vector<int>& nums) {
-13        vector<vector<int>>sol;
-14        // sort(nums.begin(), nums.end());
-15        vector<int> curr;
-16        subsets(nums, sol, curr, 0);
-17        return sol;
-18    }
-19};
+2    void subsets(vector<int>& nums, vector<vector<int>>& sol, vector<int>& curr, int i) {
+3        if (i == nums.size()) {
+4            sol.push_back(curr);
+5            return;
+6        }
+7
+8        curr.push_back(nums[i]);
+9        subsets(nums, sol, curr, i + 1);
+10        curr.pop_back();
+11
+12        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+13            i++;
+14        }
+15        subsets(nums, sol, curr, i + 1);
+16    }
+17public:
+18    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+19        vector<vector<int>> sol;
+20        vector<int> curr;
+21        sort(nums.begin(), nums.end());
+22        subsets(nums, sol, curr, 0);
+23        return sol;
+24    }
+25};
