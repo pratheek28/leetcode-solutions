4a0744ae4f0dd35e5fcd35e1742a6f1d5069a641
Pratheek Sunilkumar <66023535+pratheek28@users.noreply.github.com>
2026-07-31 17:48:16 -0700

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.cpp
@@ -0,0 +1,21 @@
+// Last updated: 7/31/2026, 5:48:16 PM
+1class Solution {
+2public:
+3    int firstMissingPositive(vector<int>& nums) {
+4        map<int, int> mp;
+5
+6        for (int num : nums) {
+7            mp[num] = 1;
+8        }
+9
+10        int ans = 1;
+11
+12        for (auto i = mp.begin(); i != mp.end(); i++) {
+13            if (ans == i->first) {
+14                ans++;
+15            }
+16        }
+17
+18        return ans;
+19    }
+20};
