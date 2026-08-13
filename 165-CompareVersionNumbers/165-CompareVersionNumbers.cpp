63982d9d652e308bf7f71249650a0dc9c0dbbae0
Pratheek Sunilkumar <66023535+pratheek28@users.noreply.github.com>
2026-08-12 20:55:35 -0700

    File updated at 8/12/2026, 8:55:34 PM

--- 165-CompareVersionNumbers/165-CompareVersionNumbers.cpp
+++ 165-CompareVersionNumbers/165-CompareVersionNumbers.cpp
@@ -1,28 +1,31 @@
-// Last updated: 8/12/2026, 8:52:18 PM
+// Last updated: 8/12/2026, 8:55:34 PM
 1class Solution {
 2public:
 3    int compareVersion(string version1, string version2) {
-4        int i = 0, j = 0;
-5
-6        while (i < version1.size() || j < version2.size()) {
-7            int num1 = 0;
-8            while (i < version1.size() && version1[i] != '.') {
-9                num1 = num1 * 10 + (version1[i] - '0');
-10                i++;
-11            }
-12
-13            int num2 = 0;
-14            while (j < version2.size() && version2[j] != '.') {
-15                num2 = num2 * 10 + (version2[j] - '0');
-16                j++;
-17            }
-18
-19            if (num1 < num2) return -1;
-20            if (num1 > num2) return 1;
-21            i++;
-22            j++;
-23        }
-24        
-25        return 0;
-26    }
-27};
+4        int idx1 = 0;
+5        int idx2 = 0;
+6
+7        while (idx1 < version1.size() || idx2 < version2.size()) {
+8            int val1 = 0;
+9            int val2 = 0;
+10
+11            while (idx1 < version1.size() && version1[idx1] != '.') {
+12                val1 = val1 * 10 + (version1[idx1] - '0');
+13                idx1++;
+14            }
+15            
+16            while (idx2 < version2.size() && version2[idx2] != '.') {
+17                val2 = val2 * 10 + (version2[idx2] - '0');
+18                idx2++;
+19            }
+20
+21
+22            if (val1 < val2) return -1;
+23            if (val2 < val1) return 1;
+24
+25            idx1++;
+26            idx2++;
+27        }
+28        return 0;
+29    }
+30};
