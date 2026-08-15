dda86ab14cb181c64accbe5fc2ab7331d38b3097
Pratheek Sunilkumar <66023535+pratheek28@users.noreply.github.com>
2026-08-14 23:13:40 -0700

    File updated at 8/14/2026, 11:13:39 PM

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.cpp
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.cpp
@@ -1,22 +1,22 @@
-// Last updated: 8/14/2026, 11:12:21 PM
+// Last updated: 8/14/2026, 11:13:39 PM
 1class Solution {
 2public:
 3    int removeDuplicates(vector<int>& nums) {
-4        unordered_map<int, int> numMap;
+4        unordered_map<int, int> mp;
 5        int size = nums.size();
 6
 7        for (int i = 0; i < size; i++) {
-8            if (numMap[nums[i]] == 2) {
+8            if (mp[nums[i]] == 2) {
 9                swap(nums[i], nums[size - 1]);
 10                size--;
 11                i--;
 12            }else {
-13                numMap[nums[i]]++;
+13                mp[nums[i]]++;
 14            }
 15        }
-16        sort(nums.begin(), nums.begin() + (size));
-17
+16
+17        sort(nums.begin(), nums.begin() + size);
 18
-19        return (size);
+19        return size;
 20    }
 21};
